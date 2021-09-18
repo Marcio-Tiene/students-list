0ce3ef52f6ab1fa08935757dd51bc7f214ac70a4
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-09-18 11:47:56 -0300

    add typeorm for postgres

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' })],
   controllers: [AppController],
   providers: [AppService],
 })
