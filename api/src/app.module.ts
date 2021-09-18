35067f0528e013da25f6c211ddb672da1a27de6a
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-09-18 11:50:56 -0300

    add typeorm module for root

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' })],
+  imports: [
+    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    TypeOrmModule.forRoot({
+      ...ormconfig,
+      autoLoadEntities: true,
+      migrationsTableName: 'migrations_typeorm',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
