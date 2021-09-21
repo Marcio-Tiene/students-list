d2873d3a84c21797b3912615828f236bbf776890
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-09-20 23:47:28 -0300

    create getservesideprops query

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { GetServerSideProps } from 'next';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
 
-export default function Home() {
-  return <Title>My page</Title>
+`
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql/',
+    cache: new InMemoryCache()
+  });
+export default function Home(props) {
+  
+  return <><Title>{props.data.students.map(student=> student.name)}</Title>
+  <button>zas</button></>
+}
+
+export const getServerSideProps : GetServerSideProps = async () => {
+  
+  
+  try{  
+    const { data, error, loading,networkStatus } = await client.query({
+      query: gql`
+      {
+        students(field: "name", searchTerms: "") {          
+          name
+          cpf
+          email
+        }
+      }
+      `
+    });
+    return {
+      props: {
+        data,
+        error:error || null,
+        loading,networkStatus
+      }
+    }
+   }catch (error) {
+     console.log(error)
+      return {
+        props:{
+          error: error.graphQLErrors[0].message
+        }
+      }
+    }
+
+ 
+  
+ 
+  
 }
