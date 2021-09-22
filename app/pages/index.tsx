ee41c3053c2d32df50dbfe941af2dc5ccecfd686
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-09-22 00:48:18 -0300

    add search engine

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,55 +1,64 @@
 import styled from 'styled-components'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import { GetServerSideProps } from 'next';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { fetchStudentsByAttributes } from '../graphql-querys/students-querys'
+import { useEffect, useState } from 'react'
+import Header from '../components/Header'
+import { useRouter } from 'next/dist/client/router'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-
 `
-const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql/',
-    cache: new InMemoryCache()
-  });
+
 export default function Home(props) {
-  
-  return <><Title>{props.data.students.map(student=> student.name)}</Title>
-  <button>zas</button></>
+  const { data, error, loading, networkStatus } = props
+
+  const router = useRouter()
+  const { query: queryparams } = router
+
+  const { field, searchTerms } = queryparams
+
+  const [query, setquery] = useState({
+    field: 'name',
+    searchTerms: searchTerms || '',
+  })
+
+  const handleTermsChange = (event) => {
+    setquery((prevState) => ({ ...prevState, searchTerms: event.target.value }))
+  }
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      const { field, searchTerms } = query
+      field === 'name' && searchTerms === ''
+        ? router.push('/')
+        : router.push(`/?field=${query.field}&searchTerms=${query.searchTerms}`)
+    }, 500)
+
+    return () => clearTimeout(handler)
+  }, [query])
+  return (
+    <>
+      <Head>
+        <title>Student lists</title>
+      </Head>
+      <Header>
+        <input
+          type="text"
+          placeholder="search"
+          value={query.searchTerms}
+          onChange={handleTermsChange}
+        />
+      </Header>
+      <Title>{props.data.students.map((student) => student.name)}</Title>
+      <button>zas</button>
+    </>
+  )
 }
 
-export const getServerSideProps : GetServerSideProps = async () => {
-  
-  
-  try{  
-    const { data, error, loading,networkStatus } = await client.query({
-      query: gql`
-      {
-        students(field: "name", searchTerms: "") {          
-          name
-          cpf
-          email
-        }
-      }
-      `
-    });
-    return {
-      props: {
-        data,
-        error:error || null,
-        loading,networkStatus
-      }
-    }
-   }catch (error) {
-     console.log(error)
-      return {
-        props:{
-          error: error.graphQLErrors[0].message
-        }
-      }
-    }
-
- 
-  
- 
-  
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { field, searchTerms } = ctx.query
+  return await fetchStudentsByAttributes({ field, searchTerms })
 }
